b1e718679b23c2cc497667c28fce602e12453020
hiraide8 <masa930831@yahoo.co.jp>
2018-10-15 21:48:59 +0900

    20181015

--- Person/src/Test.java
+++ Person/src/Test.java
@@ -11,6 +11,9 @@ public class Test {
 		System.out.println(taro.age);
 		System.out.println(taro.phoneNumber);
 		System.out.println(taro.address);
+		taro.talk();
+		taro.walk();
+		taro.run();
 
 		Person jiro = new Person();
 		jiro.name="木村次郎";
@@ -21,22 +24,59 @@ public class Test {
 		System.out.println(jiro.age);
 		System.out.println(jiro.phoneNumber);
 		System.out.println(jiro.address);
+		jiro.talk();
+		jiro.walk();
+		jiro.run();
 
 		Person hanako = new Person();
 		hanako.name="鈴木花子";
 		hanako.age=16;
 		hanako.phoneNumber="047-117-1111";
-		hanako.address="サラエボ";
+		hanako.address="千葉";
 		System.out.println(hanako.name);
 		System.out.println(hanako.age);
 		System.out.println(hanako.phoneNumber);
 		System.out.println(hanako.address);
+		hanako.talk();
+		hanako.walk();
+		hanako.run();
 
         Person masaaki = new Person();
         masaaki.name="平出雅明";
         masaaki.age=25;
+        masaaki.phoneNumber="000-0000-0000";
+        masaaki.address="神奈川";
         System.out.println(masaaki.name);
         System.out.println(masaaki.age);
+        System.out.println(masaaki.phoneNumber);
+        System.out.println(masaaki.address);
+        masaaki.talk();
+        masaaki.walk();
+        masaaki.run();
+
+        Robot aibo = new Robot();
+        aibo.name = "aibo";
+        aibo.talk();
+        aibo.walk();
+        aibo.run();
+
+        Robot asimo = new Robot();
+        asimo.name = "asimo";
+        asimo.talk();
+        asimo.walk();
+        asimo.run();
+
+        Robot pepper = new Robot();
+        pepper.name = "pepper";
+        pepper.talk();
+        pepper.walk();
+        pepper.run();
+
+        Robot doraemon = new Robot();
+        doraemon.name = "doraemon";
+        doraemon.talk();
+        doraemon.walk();
+        doraemon.run();
 
 	}
 
